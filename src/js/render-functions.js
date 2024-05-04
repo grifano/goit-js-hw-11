28a6789218fc89979ec240b2c65ad0184d28b4b6
Serhii Orlenko <146512960+grifano@users.noreply.github.com>
2024-05-04 07:53:07 +0200

    add vadiation, update ui, fetch

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,7 +1,7 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-const serachInput = document.querySelector('.ssearch-input');
+const serachInput = document.querySelector('.search-input');
 const tempPromise = true;
 
 iziToast.settings({
@@ -14,20 +14,77 @@ iziToast.settings({
 });
 
 // Function: show notification
-function showNotification(status) {
-  if (status) {
-    iziToast.error({
-      message: `Sorry, there are no images matching your search query. Please try again!`,
-      class: 'error-notification',
-      timeout: 5000,
-      iconUrl: '/img/bi_x-octagon.svg',
-      titleColor: '#fff',
-      position: 'topRight',
-      backgroundColor: '#EF4040',
-      messageColor: '#fff',
-      progressBarColor: '#B51B1B',
-      close: true,
-    });
+export function showNotification() {
+  iziToast.error({
+    message: `Sorry, there are no images matching your search query. Please try again!`,
+    class: 'error-notification',
+    timeout: 5000,
+    iconUrl: '/img/bi_x-octagon.svg',
+    titleColor: '#fff',
+    position: 'topRight',
+    backgroundColor: '#EF4040',
+    messageColor: '#fff',
+    progressBarColor: '#B51B1B',
+    close: true,
+  });
+}
+
+// Update UI
+export function updateUi(arrayImages) {
+  if (arrayImages.length <= 0) {
+    showNotification();
+  }
+  const gallery = document.querySelector('.gallery-list');
+  const markup = arrayImages
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<li class="image-card">
+              <img src="${webformatURL}" width="360" height="200" class="image-card__thumb" alt="${tags}">
+              <ul class="image-card__footer">
+                <li>
+                    <p>Likes</p>
+                    <p>${likes}</p>
+                </li>
+                <li>
+                    <p>Views</p>
+                    <p>${views}</p>
+                </li>
+                <li>
+                    <p>Comments</p>
+                    <p>${comments}</p>
+                </li>
+                <li>
+                    <p>Downloads</p>
+                    <p>${downloads}</p>
+                </li>
+              </ul>
+          </li>`;
+      }
+    )
+    .join('');
+  gallery.insertAdjacentHTML('afterbegin', markup);
+}
+
+// Check user input
+export function getUserValue(event) {
+  const button = document.querySelector('button');
+  const value = event.target.value;
+
+  if (value) {
+    button.classList.remove('is-disable');
+    button.removeAttribute('disabled', '');
+    return value;
+  } else {
+    button.classList.add('is-disable');
+    button.setAttribute('disabled', '');
   }
   return;
 }
--- src/main.js
+++ src/main.js
@@ -1,2 +1,31 @@
 import './js/pixabay-api';
 import './js/render-functions';
+import { updateUi, getUserValue } from './js/render-functions';
+
+const refs = {
+  searchInput: document.querySelector('#search-input'),
+  searchButton: document.querySelector('button'),
+};
+let userSearchRequestValue = '';
+
+refs.searchInput.addEventListener('input', event => {
+  userSearchRequestValue = getUserValue(event);
+});
+refs.searchButton.addEventListener('click', () => {
+  fetchImageData(userSearchRequestValue);
+});
+
+function fetchImageData(serachRequest) {
+  const apiRequestURL = `https://pixabay.com/api/?key=20858658-55430aeeed6a37ac1f56d3c0c&q=${serachRequest}&image_type=photo&orientation=horizontal&safesearch=true`;
+  return fetch(apiRequestURL)
+    .then(response => {
+      if (!response.ok) {
+        return;
+      }
+      return response.json();
+    })
+    .then(data => {
+      const images = data.hits;
+      updateUi(images);
+    });
+}
